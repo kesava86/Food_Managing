c208a6042a43ff158c0516d8a2d27688fa2e76b9
kesava <145857768+kesava86@users.noreply.github.com>
2025-06-24 13:18:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
@@ -7,16 +6,15 @@ import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
-import Verify from "./pages/Verify/verify";
+import Verify from "./pages/Verify/Verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
-
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
@@ -33,38 +31,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/Sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
-import Add from './pages/Add/Add'
-import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-
-const App = () => {
-  const url = "http://localhost:4000"
-
-  return (
-    <div>
-      <ToastContainer/>
-      <Navbar/>
-      <hr />
-      <div className="app-content">
-        <Sidebar/>
-        <Routes>
-          <Route path='/add' element={<Add url={url}/>}/>
-          <Route path='/list' element={<List url={url}/>}/>
-          <Route path='/orders' element={<Orders url={url}/>}/>
-
-        </Routes>
-      </div>
-
-    </div>
-  )
-}
-
-export default App
->>>>>>> d617b805405519231c46dd521d274804f72b6485
