1a3cecd8bc80c8a9caa652b7aefcb7993668dac2
kesava <145857768+kesava86@users.noreply.github.com>
2025-06-24 13:17:06 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-<<<<<<< HEAD
-import {BrowserRouter} from 'react-router-dom'
-import StoreContextProvider from './context/StoreContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import StoreContextProvider from './context/StoreContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-
-    </StoreContextProvider>
-  </BrowserRouter>
-=======
-import {BrowserRouter} from "react-router-dom"
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-    
->>>>>>> d617b805405519231c46dd521d274804f72b6485
-  
-)
+  <StrictMode>
+    <BrowserRouter>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
