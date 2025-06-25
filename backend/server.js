fffcea64a637702ee906873e7064942c0d051fcb
kesava <145857768+kesava86@users.noreply.github.com>
2025-06-25 12:19:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,5 +33,3 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server Started on http://localhost:${port}`)
 })
-
-// mongodb+srv://kkesava493:X9XD6jtt3eQTnIbl@cluster0.5hwbqv5.mongodb.net/?
