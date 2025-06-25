6f1c9bfa31524473f01f257810de46c20eb110c9
kesava <145857768+kesava86@users.noreply.github.com>
2025-06-25 12:29:52 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://render.com/docs/web-services#port-binding";
+  const url = "https://food-managing.onrender.com";
 
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
